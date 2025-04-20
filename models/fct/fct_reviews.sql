6108bf52f1818d15376a0211ace3effdde7af039
Srimanta Pal <srimantapal205@hotmail.com>
2025-04-20 19:17:35 +0530

    Add dim file and add fact file

---
+++ models/fct/fct_reviews.sql
@@ -0,0 +1,15 @@
+{{
+  config(
+    materialized = 'incremental',
+    on_schema_change='fail'
+    )
+}}
+WITH src_reviews AS (
+    SELECT * FROM {{ref("src_reviews")}}
+)
+SELECT * FROM src_reviews 
+WHERE
+    review_txt IS NOT NULL
+{% if is_incremental() %}
+  AND review_date > (select max(review_date) from {{ this }})
+{% endif %}
