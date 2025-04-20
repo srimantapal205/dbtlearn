75a0a2055ec57d88015fa2734a832a60659ff9ab
Srimanta Pal <srimantapal205@hotmail.com>
2025-04-20 19:18:44 +0530

    Add revierwer name column

--- models/src/src_reviews.sql
+++ models/src/src_reviews.sql
@@ -4,6 +4,7 @@ WITH raw_reviews AS (
 SELECT 
      listing_id,
      date AS review_date,
+     reviewer_name AS reviewer_name,
      comments AS review_txt,
      sentiment AS review_sentiment
 FROM
